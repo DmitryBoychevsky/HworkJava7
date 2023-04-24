d20a3216a4033b03f03b32963cbac64ec5da85ee
Dmitry Boychevsky <javashdrug@yandex.ru>
2023-04-24 13:17:03 +0300

    Add test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,15 +1,29 @@
 package ru.netology.statistic;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+
 public class StatisticsServiceTest {
     @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
 
+        long expected = 12;
         long actual = service.findMax(incomesInBillions);
         Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void ZeroMinValuesNegative() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {0, 9, 5, -1, 11, 0, -11, 1, 11, 11, 10}; // нолевые, отрицательные, 2 ожидаемых подряд
+
+        long expected = 11;
+        long actual = service.findMax(incomesInBillions);
+        Assertions.assertEquals(expected, actual);
+
+
     }
 }
 
